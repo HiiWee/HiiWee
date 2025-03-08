f873096e75f4044988406328832c3f717e6a21a1
Hoseok Lee <wpdlzhf159@gmail.com>
2025-03-08 21:24:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,13 @@ MAX_POST = 5
 
 markdown_text = """
 ### Hi there 👋
-백엔드 공부를 하고 있는 학생입니다.   
-좋은 구조, 코드란 무엇인지 궁금해하며   
-그것들을 판단할 수 있는 능력을 가지기 위해 노력 중 입니다.   
-`Graceful code comes from hard work!!!`
 
-🌱 I’m currently learning ...
+Hi, I'm Hoseok Lee, a passionate backend developer!
+Always open to learning and collaborating, so feel free to reach out!
 
-<a href="https://www.java.com/ko/" target="_blank"><img src="https://img.shields.io/badge/Java-007396?style=flat-square&logo=Java&logoColor=white"/></a>
-<a href="https://spring.io/" target="_blank"><img src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"/></a>
+- [e-mail](wpdlzhf159@gmail.com) 
+- [LinkedIn](https://www.linkedin.com/in/%ED%98%B8%EC%84%9D-%EC%9D%B4-01278b264/)
 
-
-![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=HiiWee&&show_icons=true&theme=highcontrast)
-<!--
-**HiiWee/HiiWee** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-
-Here are some ideas to get you started:
-
-- 🔭 I’m currently working on ...
-
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
 
 ### 📌 Latest Blog Post
 
