e2fd290bc8c207e73bdefd074dc11cc9a6e76cd5
Hoseok Lee <wpdlzhf159@gmail.com>
2025-03-14 09:50:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ markdown_text = """
 Hi, I'm Hoseok Lee, a passionate backend developer!
 Always open to learning and collaborating, so feel free to reach out!
 
-- [e-mail](wpdlzhf159@gmail.com) 
+- [e-mail](mailto:wpdlzhf159@gmail.com) 
 - [LinkedIn](https://www.linkedin.com/in/%ED%98%B8%EC%84%9D-%EC%9D%B4-01278b264/)
 
 
